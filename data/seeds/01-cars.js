e31f4074bbd98aae3c14cfe58a40797f9d7a5584
sydney.hopson <sydneyhopsonreignmobile@Sydneys-iMac.local>
2023-05-29 10:32:09 -0400

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,34 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'LR4',
+        model: 'LandRover',
+        milage: 140000,
+        title: 'clean',
+        transmission: 'automatic',
+    },
+    {
+        vin: '1111111111111',
+        make: 'Model Y',
+        model: 'Tesla',
+        milage: 60000,
+        title: 'salvage',
+        transmission: 'automatic',
+    },
+    {
+        vin: '1111111111111',
+        make: 'EV',
+        model: 'Hummer',
+        milage: 1600,
+        title: 'clean',
+        
+    },
+
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars) 
+    
+}
